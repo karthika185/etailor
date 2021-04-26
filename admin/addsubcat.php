9373d8c8dd6a09cc91435fecb7e01a7531f7ee20
Alex Jose <alexjose9048@gmail.com>
2021-04-26 23:14:19 +0530

    Formatted code

--- admin/addsubcat.php
+++ admin/addsubcat.php
@@ -53,133 +53,157 @@ if(isset($_POST["add"]))
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Subcategory page</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <style>
-      * {
-      box-sizing: border-box;
-      }
-      html, body {
-      min-height: 100vh;
-      padding: 0;
-      margin: 0;
-      font-family: Roboto, Arial, sans-serif;
-      font-size: 14px;
-      color: #666;
-      }
-      input, textarea {
-      outline: none;
-      }
-      .section-1 {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 20px;
-      background: #f5d09a;
-      }
-      h1 {
-      margin-top: 0;
-      font-weight: 500;
-      }
-      form {
-      position: relative;
-      width: 80%;
-      border-radius: 30px;
-      background: #fff;
-      }
-      .form-left-decoration,
-      .form-right-decoration {
-      content: "";
-      position: absolute;
-      width: 50px;
-      height: 20px;
-      border-radius: 20px;
-      background: #f5d09a;
-      }
-      .form-left-decoration {
-      bottom: 60px;
-      left: -30px;
-      }
-      .form-right-decoration {
-      top: 60px;
-      right: -30px;
-      }
-      .form-left-decoration:before,
-      .form-left-decoration:after,
-      .form-right-decoration:before,
-      .form-right-decoration:after {
-      content: "";
-      position: absolute;
-      width: 50px;
-      height: 20px;
-      border-radius: 30px;
-      background: #fff;
-      }
-      .form-left-decoration:before {
-      top: -20px;
-      }
-      .form-left-decoration:after {
-      top: 20px;
-      left: 10px;
-      }
-      .form-right-decoration:before {
-      top: -20px;
-      right: 0;
-      }
-      .form-right-decoration:after {
-      top: 20px;
-      right: 10px;
-      }
-      .circle {
-      position: absolute;
-      bottom: 80px;
-      left: -55px;
-      width: 20px;
-      height: 20px;
-      border-radius: 50%;
-      background: #fff;
-      }
-      .form-inner {
-      padding: 40px;
-      }
-      .form-inner input,
-      .form-inner textarea {
-      display: block;
-      width: 100%;
-      padding: 15px;
-      margin-bottom: 10px;
-      border: none;
-      border-radius: 20px;
-      background: #d0dfe8;
-      }
-      .form-inner textarea {
-      resize: none;
-      }
-      .button {
-      width: 100%;
-      padding: 10px;
-      margin-top: 20px;
-      border-radius: 20px;
-      border: none;
-      border-bottom: 4px solid #ffb03b;
-      background: #ffb03b;
-      font-size: 16px;
-      font-weight: 400;
-      color: #fff;
-      }
-      .button:hover {
-      background: #ffb03b;
-      }
-      @media (min-width: 568px) {
-      form {
-      width: 60%;
-      }
-      }
+    * {
+        box-sizing: border-box;
+    }
+
+    html,
+    body {
+        min-height: 100vh;
+        padding: 0;
+        margin: 0;
+        font-family: Roboto, Arial, sans-serif;
+        font-size: 14px;
+        color: #666;
+    }
+
+    input,
+    textarea {
+        outline: none;
+    }
+
+    .section-1 {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+        background: #f5d09a;
+    }
+
+    h1 {
+        margin-top: 0;
+        font-weight: 500;
+    }
+
+    form {
+        position: relative;
+        width: 80%;
+        border-radius: 30px;
+        background: #fff;
+    }
+
+    .form-left-decoration,
+    .form-right-decoration {
+        content: "";
+        position: absolute;
+        width: 50px;
+        height: 20px;
+        border-radius: 20px;
+        background: #f5d09a;
+    }
+
+    .form-left-decoration {
+        bottom: 60px;
+        left: -30px;
+    }
+
+    .form-right-decoration {
+        top: 60px;
+        right: -30px;
+    }
+
+    .form-left-decoration:before,
+    .form-left-decoration:after,
+    .form-right-decoration:before,
+    .form-right-decoration:after {
+        content: "";
+        position: absolute;
+        width: 50px;
+        height: 20px;
+        border-radius: 30px;
+        background: #fff;
+    }
+
+    .form-left-decoration:before {
+        top: -20px;
+    }
+
+    .form-left-decoration:after {
+        top: 20px;
+        left: 10px;
+    }
+
+    .form-right-decoration:before {
+        top: -20px;
+        right: 0;
+    }
+
+    .form-right-decoration:after {
+        top: 20px;
+        right: 10px;
+    }
+
+    .circle {
+        position: absolute;
+        bottom: 80px;
+        left: -55px;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background: #fff;
+    }
+
+    .form-inner {
+        padding: 40px;
+    }
+
+    .form-inner input,
+    .form-inner textarea {
+        display: block;
+        width: 100%;
+        padding: 15px;
+        margin-bottom: 10px;
+        border: none;
+        border-radius: 20px;
+        background: #d0dfe8;
+    }
+
+    .form-inner textarea {
+        resize: none;
+    }
+
+    .button {
+        width: 100%;
+        padding: 10px;
+        margin-top: 20px;
+        border-radius: 20px;
+        border: none;
+        border-bottom: 4px solid #ffb03b;
+        background: #ffb03b;
+        font-size: 16px;
+        font-weight: 400;
+        color: #fff;
+    }
+
+    .button:hover {
+        background: #ffb03b;
+    }
+
+    @media (min-width: 568px) {
+        form {
+            width: 60%;
+        }
+    }
     </style>
 </head>
+
 <body>
     <input type="checkbox" id="checkbox">
     <header class="header">
@@ -217,13 +241,13 @@ if(isset($_POST["add"]))
                         <span>Customer Enquiry</span>
                     </a>
                 </li>
-                 <li>
+                <li>
                     <a href="btqenq.php">
                         <i class="fa fa-envelope" aria-hidden="true"></i>
                         <span>Boutique Enquiry</span>
                     </a>
                 </li>
-                
+
                 <li>
                     <a href="addcat.php">
                         <i class="fa fa-plus" aria-hidden="true"></i>
@@ -257,42 +281,36 @@ if(isset($_POST["add"]))
             </ul>
         </nav>
         <section class="section-1">
-          <center><p style="color: black;font-family:satisfy">SUBCATEGORY</p></center><br><br>
-                <form action="" method="post" class="decor" enctype="multipart/form-data">
-      <div class="form-left-decoration"></div>
-      <div class="form-right-decoration"></div>
-      <div class="circle"></div>
-      <div class="form-inner">
+            <center>
+                <p style="color: black;font-family:satisfy">SUBCATEGORY</p>
+            </center><br><br>
+            <form action="" method="post" class="decor" enctype="multipart/form-data">
+                <div class="form-left-decoration"></div>
+                <div class="form-right-decoration"></div>
+                <div class="circle"></div>
+                <div class="form-inner">
 
-                
-                <label>CATEGORY</label>
+
+                    <label>CATEGORY</label>
                     <?php
                     $cats=$dao->createOptions("cat_name","cat_id","tbl_category");
                     echo $form->dropDownList("cat_name",array("class"=>"form-input","select"),$cats,"Select Category");
                     ?><br><br><br>
-                <label>SUBCATEGORY NAME</label>
-                <?php echo $form->textBox("subcategory_name",array("class"=>"field","placeholder"=>"SUBCATEGORY NAME")); ?><br><br>
-            <label>SUBCATEGORY PICTURE</label>
-                <?php echo $form->fileField("subcategory_pic",array("placeholder"=>"SUBCATEGORY PICTURE")); ?>
-               
-            
-        
-        <input type="submit" name="add" value="SUBMIT" class="button"/>
-        
-        </div>
-        <h4><?php echo isset($msg)?$msg:"";?></h4>
-    </form>
+                    <label>SUBCATEGORY NAME</label>
+                    <?php echo $form->textBox("subcategory_name",array("class"=>"field","placeholder"=>"SUBCATEGORY NAME")); ?><br><br>
+                    <label>SUBCATEGORY PICTURE</label>
+                    <?php echo $form->fileField("subcategory_pic",array("placeholder"=>"SUBCATEGORY PICTURE")); ?>
+
+
+
+                    <input type="submit" name="add" value="SUBMIT" class="button" />
+
+                </div>
+                <h4><?php echo isset($msg)?$msg:"";?></h4>
+            </form>
         </section>
     </div>
 
 </body>
+
 </html>
-
-
-
-
-
-
-
-
-
--- btq/request.php
+++ btq/request.php
@@ -5,75 +5,78 @@ $btq_id=$_SESSION["btq_id"];
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>boutique home</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/style.css">
+    <title>boutique home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-	<input type="checkbox" id="checkbox">
-	<header class="header">
-		<h2 class="u-name">e<b>Tailoring</b>
-			<label for="checkbox">
-				<i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
-			</label>
-		</h2>
-		<i class="fa fa-user" aria-hidden="true"></i>
-	</header>
-	<div class="body">
-		<nav class="side-bar">
-			<div class="user-p">
-				<img src="img/user.jpg">
-				<h4><?php echo $_SESSION["btq_name"];?></h4>
-			</div>
-			<ul>
-				<li>
-					<a href="viewbtqprofile.php">
-						<i class="fa fa-desktop" aria-hidden="true"></i>
-						<span>Profile</span>
-					</a>
-				</li>
-				<li>
-					<a href="addmaterial.php">
-						<i class="fa fa-plus-square" aria-hidden="true"></i>
-						<span>Add Materials</span>
-					</a>
-				</li>
-				<li>
-					<a href="addproduct.php">
-						<i class="fa fa-plus-square" aria-hidden="true"></i>
-						<span>Add Products</span>
-					</a>
-				</li>
-				<li>
-					<a href="request.php">
-						<i class="fa fa-check-square-o" aria-hidden="true"></i>
-						<span>Requests</span>
-					</a>
-				</li>
-				<li>
-					<a href="order.php">
-						<i class="fa fa-cog" aria-hidden="true"></i>
-						<span>Orders</span>
-					</a>
-				</li>
-				<li>
-					<a href="btqnot.php">
-						<i class="fa fa-bell" aria-hidden="true"></i>
-						<span>Notification</span>
-					</a>
-				</li>
-				<li>
-					<a href="../destroysession.php">
-						<i class="fa fa-power-off" aria-hidden="true"></i>
-						<span>Logout</span>
-					</a>
-				</li>
-			</ul>
-		</nav>
-		
-		
-	</div>
+    <input type="checkbox" id="checkbox">
+    <header class="header">
+        <h2 class="u-name">e<b>Tailoring</b>
+            <label for="checkbox">
+                <i id="navbtn" class="fa fa-bars" aria-hidden="true"></i>
+            </label>
+        </h2>
+        <i class="fa fa-user" aria-hidden="true"></i>
+    </header>
+    <div class="body">
+        <nav class="side-bar">
+            <div class="user-p">
+                <img src="img/user.jpg">
+                <h4><?php echo $_SESSION["btq_name"];?></h4>
+            </div>
+            <ul>
+                <li>
+                    <a href="viewbtqprofile.php">
+                        <i class="fa fa-desktop" aria-hidden="true"></i>
+                        <span>Profile</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="addmaterial.php">
+                        <i class="fa fa-plus-square" aria-hidden="true"></i>
+                        <span>Add Materials</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="addproduct.php">
+                        <i class="fa fa-plus-square" aria-hidden="true"></i>
+                        <span>Add Products</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="request.php">
+                        <i class="fa fa-check-square-o" aria-hidden="true"></i>
+                        <span>Requests</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="order.php">
+                        <i class="fa fa-cog" aria-hidden="true"></i>
+                        <span>Orders</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="btqnot.php">
+                        <i class="fa fa-bell" aria-hidden="true"></i>
+                        <span>Notification</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="../destroysession.php">
+                        <i class="fa fa-power-off" aria-hidden="true"></i>
+                        <span>Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+
+    </div>
 
 </body>
+
 </html>
