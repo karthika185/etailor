00976a8c611f55e8eb824579e0980f604dae3b02
Alex Jose <alexjose9048@gmail.com>
2021-05-05 00:08:44 +0530

    Added Material Feature

---
+++ cust/get_material.php
@@ -0,0 +1,20 @@
+<?Php
+@$btq_id=$_GET['btq_id'];
+//$cat_id=2;
+/// Preventing injection attack //// 
+if(!is_numeric($btq_id)){
+echo "Data Error";
+exit;
+ }
+/// end of checking injection attack ////
+require "config.php"; // Database connection string 
+
+$sql="SELECT tbl_material.mat_name,tbl_material.mat_id,tbl_material.btq_id,tbl_btqreg.btq_id FROM tbl_material INNER JOIN tbl_btqreg ON tbl_material.btq_id=tbl_btqreg.btq_id WHERE tbl_material.btq_id= :btq_id";
+$row=$dbo->prepare($sql);
+$row->bindParam(':btq_id',$btq_id,PDO::PARAM_INT,5);
+$row->execute();
+$result=$row->fetchAll(PDO::FETCH_ASSOC);
+
+$main = array('data'=>$result);
+echo json_encode($main);
+?>
