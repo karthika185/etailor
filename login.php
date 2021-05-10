eccf5273e3bd57f94a9bc2c1a664c81d37f85886
karthikasuresh <57707040+karthika185@users.noreply.github.com>
2021-05-10 22:06:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -43,7 +43,7 @@ if(isset($_POST["log"]))
         {
           $_SESSION["C"]=$_POST["email"];
           $data=$dao->getData("*","tbl_custreg","cust_email='".$_POST["email"]."'");
-          $_SESSION["cust_id"]=$data["cust_id"];
+          $_SESSION["cust_id"]=$data[0]["cust_id"];
           $_SESSION["cust_name"]=$data[0]["cust_name"];
           $_SESSION["cust_email"]=$data[0]["cust_email"];
           $_SESSION["cust_phone"]=$data[0]["cust_phone"];
