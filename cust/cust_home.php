872df4bf99f5cf81275220091fef0bfee03c4ae1
Alex Jose <alexjose9048@gmail.com>
2021-06-03 12:52:33 +0530

    Changed UI for customer home page

--- cust/cust_home.php
+++ cust/cust_home.php
@@ -4,139 +4,80 @@ $cust_id=$_SESSION["cust_id"];
 $cust_email=$_SESSION["cust_email"];
 if(!isset($_SESSION['cust_name']))
 {
-header('location:../login.php');
+    header('location:../login.php');
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>e-Tailoring</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+  <title>e-tailoring system</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    <!-- Favicons -->
-    <link href="assets/img/favicon.png" rel="icon">
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <!-- Favicons -->
+  <link href="asset/img/favicon.png" rel="icon">
+  <link href="asset/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-    <!-- Google Fonts -->
-    <link
-        href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i"
-        rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="asset/animate.css/animate.min.css" rel="stylesheet">
-    <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="asset/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="asset/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="asset/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="asset/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  <link href="asset/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="asset/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="asset/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="asset/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="asset/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="asset/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
-    <link href="style.css" rel="stylesheet">
-
-    <!-- =======================================================
-  * Template Name: Delicious - v4.1.0
-  * Template URL: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
+  <!-- Template Main CSS File -->
+  <link href="asset/css/style.css" rel="stylesheet">
 </head>
 
 <body>
 
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container">
 
+      <h1><a href="cust_home2.php">Hi <?php echo $_SESSION["cust_name"];?> !</a></h1>
+      <h2>Welcome to <span>e-Tailoring</span></h2>
 
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top d-flex align-items-center ">
-        <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link" href="#profile">Profile</a></li>
+          <li><a class="nav-link" href="notify.php">Notification</a></li>
+          <li><a class="nav-link" href="customiseform1.php">Send Global Request</a></li>
+          <li><a class="nav-link" href="customiseform.php">Send Boutique Request</a></li>
+          <li><a class="nav-link" href="#search">Search</a></li>
+          <li><a class="nav-link" href="#help">Help</a></li>
+          <li><a class="nav-link" href="#Logout">Logout</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
 
-            <div class="logo me-auto">
-                <h1><a href="index.html">e-Tailoring</a></h1>
-                <!-- Uncomment below if you prefer to use an image logo -->
-                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-            </div>
+      <div class="social-links">
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+      </div>
 
-            <nav id="navbar" class="navbar order-last order-lg-0">
-                <ul>
-                    <li><a class="nav-link scrollto " href="notify.php">Notification</a></li>
-                    <li><a class="nav-link scrollto " href="#hero">Home</a></li>
-                    <li class="dropdown"><a href="#"><span>Customise</span> </a>
-                        <ul>
+    </div>
+  </header><!-- End Header -->
 
+  <!-- Vendor JS Files -->
+  <script src="asset/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="asset/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="asset/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="asset/vendor/php-email-form/validate.js"></script>
+  <script src="asset/vendor/purecounter/purecounter.js"></script>
+  <script src="asset/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="asset/vendor/waypoints/noframework.waypoints.js"></script>
 
-                            <li><a href="customiseform1.php">Send Request</a></li>
-                            <li><a href="customiseform.php">Choose Boutique</a></li>
-                        </ul>
-
-                    
-                    <li><a class="nav-link scrollto" href="#menu">Search</a></li>
-                    <li><a class="nav-link scrollto" href="#specials">Help</a></li>
-                    <li class="dropdown"><a href="#"><span><?php echo $_SESSION["cust_email"];?></span> </a>
-                        <ul>
-
-
-                            <li><a href="#">Profile</a></li>
-                            <li><a href="#">Logout</a></li>
-                        </ul>
-
-
-
-                </ul>
-                <i class="bi bi-list mobile-nav-toggle"></i>
-            </nav><!-- .navbar -->
-
-
-
-        </div>
-    </header><!-- End Header -->
-
-    <main id="main">
-
-        <section></section>
-
-
-    </main><!-- End #main -->
-
-    <!-- ======= Footer ======= -->
-    <footer id="footer">
-        <div class="container">
-            <h3>e-Tailoring</h3>
-
-            <div class="social-links">
-
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-
-            </div>
-            <div class="copyright">
-                &copy; Copyright <strong><span>e-Tailoring</span></strong>. All Rights Reserved
-            </div>
-            <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/delicious-free-restaurant-bootstrap-theme/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-            </div>
-        </div>
-    </footer><!-- End Footer -->
-
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
-
-    <!-- Vendor JS Files -->
-    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-    <script src="assets/vendor/php-email-form/validate.js"></script>
-    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-
-    <!-- Template Main JS File -->
-    <script src="assets/js/main.js"></script>
+  <!-- Template Main JS File -->
+  <script src="asset/js/main.js"></script>
 
 </body>
 
